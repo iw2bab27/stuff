668976222be3c5cdc070fd3fec0c1b5e57ae4f05
iw2bab27 <91477152+iw2bab27@users.noreply.github.com>
2021-12-20 16:26:39 +1100

    Add files via upload

---
+++ render.js
@@ -0,0 +1,6 @@
+const menuShowHide = () => {
+  const element2 = document.getElementById('icon')
+  const element = document.getElementById('menu');
+  element.classList.toggle('menu')
+  element2.classList.toggle('iconChange')
+}
